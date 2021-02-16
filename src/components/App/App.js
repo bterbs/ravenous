ec2d74b66afe2d2c5450adf998eea38d3b1ae0c8
Brittany <brterbush@zappos.com>
2021-02-16 14:15:56 -0800

    Add searchbar state.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import "./App.css";
-import BusinessList from "../BusinessList/BusinessList";
-import SearchBar from "../SearchBar/SearchBar";
+import React from "react"
+import "./App.css"
+
+import BusinessList from "../BusinessList/BusinessList"
+import SearchBar from "../SearchBar/SearchBar"
 
 const business = {
   imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
@@ -12,21 +13,25 @@ const business = {
   zipCode: "10101",
   category: "Italian",
   rating: 4.5,
-  reviewCount: 90,
-};
+  reviewCount: 90
+}
 
-const businesses = [business, business, business, business, business, business];
+const businesses = [business, business, business, business, business, business]
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,17 +1,18 @@
-import React from "react";
-import "./BusinessList.css";
-import Business from "../Business/Business";
+import React from "react"
+import "./BusinessList.css"
+
+import Business from "../Business/Business"
 
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {this.props.businesses.map((business) => (
-          <Business business={business} />
-        ))}
+        {this.props.businesses.map((business) => {
+          return <Business business={business} />
+        })}
       </div>
-    );
+    )
   }
 }
 
-export default BusinessList;
+export default BusinessList
